ce38d6920c68a087bb04cc6532f2a37b7f8f06ff
F4NT0 <gabriel.stundner@acad.pucrs.br>
2019-03-08 04:36:51 -0300

    codigo da lili

---
+++ Exercicios/Love_Lili/love_lili.py
@@ -0,0 +1,12 @@
+# Criando um while infinito do meu amor pela Lili
+from time import sleep
+import os 
+
+amor = 1
+lili = 1000000 
+while amor <= lili:
+    print('Fanto deu ',amor,' beijos em sua rainha!')
+    amor += 1 # aumenta o meu amor
+    lili += 10000 # aumenta o amor da lili
+    sleep(1) # tempo entre beijos
+    os.system('cls')
