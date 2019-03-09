6c0f15c86b81ae05c8ba13e9dcb711fdf9c101e5
F4NT0 <gabriel.stundner@acad.pucrs.br>
2019-03-09 12:03:19 -0300

    if/else em uma lista

--- Basics/If_else/if_else_lista.py
+++ Basics/If_else/if_else_lista.py
@@ -2,9 +2,14 @@
 
 nomes = ['Gabriel','Pedro','Vini','Dodo']
 meu_nome = input('Seu nome e? ')
+contador = 0 # para finalizar a passagem dentro do for
 for nome in nomes:
-    if meu_nome in nome:
-        print('meu nome esta na lista!')
+    if meu_nome in nome: # se tiver o nome na lista
+         print('meu nome esta na lista!')
+         break # finaliza o for
+    elif contador == 3 and meu_nome not in nome: # se não tiver
+         print('Nao estou na lista!')
     else:
-        pass
-    
+         pass
+    contador += 1
+# Quando o contador chega no tamanho da lista, testa o elif
