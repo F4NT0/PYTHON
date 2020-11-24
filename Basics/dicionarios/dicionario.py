57c1abfe67657461f4faa4b1a7e7c55eeef863f8
F4NT0 <gabriel.stundner@acad.pucrs.br>
2020-11-23 23:18:37 -0300

    Inicio do Dicionario

--- Basics/dicionarios/dicionario.py
+++ Basics/dicionarios/dicionario.py
@@ -1,21 +1,27 @@
-dicionario = {
-    1 : 1,
-    2 : 2,
-    3 : 3,
-    4 : 4
+#
+# Teste de Dicionários
+#
+
+# Pegando dados do dicionário pela chave
+
+dic = {
+    'Color': 'Green',
+    'Pepe': 1,
+    True: 5
 }
-print(dicionario) # saida: {1,2,3,4}
 
-# Alterando valores
-dicionario[1] = 0
-dicionario[2] = 1
-dicionario[3] = 2
-dicionario[4] = 3
-print(dicionario) # saida: {0,1,2,3}
+print("Primeiro dado do Dicionario: " + dic['Color'])
+print("Segundo dado do Dicionario: " + str(dic['Pepe']))
+print("Terceiro dado do Dicionario: " + str(dic[True]))
 
-# Adicionando valores
-dicionario[5] = 4
-dicionario[6] = 5
-dicionario[7] = 6
-dicionario[8] = 7
-print(dicionario) # saida: {0,1,2,3,4,5,6,7}
+# Definindo todas as Chaves como Numeros
+
+dic2 = {
+    1 : 3,
+    2 : 4,
+    3 : 5 
+}
+
+print("\nPrimeiro Valor do Dic2: " + str(dic2[1]))
+print("Segundo Valor do Dic2: " + str(dic2[2]))
+print("Terceiro Valor do Dic2: " + str(dic2[3]))
